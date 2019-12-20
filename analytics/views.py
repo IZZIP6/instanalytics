b960f7717692fa6e3206add8140693455931fd40
Sara Pizzimenti <pizzimentisara@gmail.com>
2019-12-20 16:27:55 +0100

    handled http 404 response

--- analytics/views.py
+++ analytics/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from analytics.app import prova
 
 def index(request):
@@ -10,6 +10,6 @@ def start(request, username_id):
         context = prova.insert_username(username_id)
         return render(request, 'analytics/user.html', context)
     except:
-        return HttpResponse('User not found, try another username')
+        return HttpResponseNotFound('User not found, try another username')
 
 
