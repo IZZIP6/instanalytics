7153d63d15419e41ba8d9d080d21a9d948de71c6
SaraPizzi <sara.pizzimenti01@universitadipavia.it>
2020-02-20 13:16:01 +0100

    todo receives username from a queue

--- todo-flask/app/route.py
+++ todo-flask/app/route.py
@@ -1,6 +1,25 @@
 from app import app
 from app import start
+import pika
+import time
 
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='username_queue', durable=True)
+print(' [*] Waiting for messages. To exit press CTRL+C')
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %s" % body)
+    time.sleep(1)
+    print(" [x] Done")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='task_queue', on_message_callback=callback)
+
+channel.start_consuming()
 
 @app.route("/s/<username>")                   # at the end point /
 def hello(username):                      # call method hello
