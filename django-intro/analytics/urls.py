f86f4d87e00cbeafec1a5a79af767012cf8bd013
SaraPizzi <sara.pizzimenti01@universitadipavia.it>
2020-03-01 17:27:03 +0100

    new path for auth

--- django-intro/analytics/urls.py
+++ django-intro/analytics/urls.py
@@ -3,5 +3,8 @@ from .import views
 
 urlpatterns = [
         path('', views.index, name='index'),
+        path('register', views.register,name='register'),
+        path('login', views.log,name='login'),
+        path('logout', views.logoutUser,name='logout'),
         path('<str:username_id>', views.start, name='start'),
         ]
--- django-intro/analytics/views.py
+++ django-intro/analytics/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 import requests
+
 
 def index(request):
     return render(request, 'analytics/index.html')
+
 
 def start(request, username_id):
     try:
@@ -13,3 +17,42 @@ def start(request, username_id):
         return render(request, 'analytics/err.html')
 
 
+def log(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        if request.method == "POST":
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user     = authenticate(request, username=username,password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+            else:
+                messages.info(request, 'Username or Password is incorrect')
+        context = {}
+        return render(request, 'analytics/login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect(index)
+
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        if request.method == "POST":
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get('username')
+                confirm  = "Account created for {username}"
+                return render(request, 'analytics/register.html', {'confirm': confirm, 'form': form})
+            else:
+                err = form.errors
+                return render(request, 'analytics/register.html', {'err': err, 'form': form})
+        else:
+            form = UserCreationForm()
+        return render(request, 'analytics/register.html', {"form": form})
